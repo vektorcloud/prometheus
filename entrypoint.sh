ea9cfea03c0ba77d7795578a18e3f1f319962746
Bradley Cicenas <bradley@vektor.nyc>
2017-08-16 13:03:38 +0000

    ensure run dir is created

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ echo "$BASIC_AUTH" > /etc/nginx/auth.htpasswd
   sed -i "s/auth.*//g" /etc/nginx/conf.d/proxy.conf
 }
 
+mkdir -p /run/nginx
 nginx -g 'daemon off; error_log  /dev/stderr info;' &
 
 exec /bin/prometheus "$@"
